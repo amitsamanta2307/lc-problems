e5fd6478807201a5d788b3b0368da59241c3d988
Amit Samanta <developer.amitsamanta@gmail.com>
2020-10-19 00:24:19 +0530

    add logic for the first problem

---
+++ LCChallengeSolutions/LCChallengeSolutions/LCProblem121.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCChallengeSolutions
+{
+    /*
+     * Problem Statement
+     * Problem 121. 
+     * ### Best Time to Buy and Sell Stock
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     * If you were only permitted to complete at most one transaction 
+     * (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+     * Note that you cannot sell a stock before you buy one.
+     */
+    public class LCProblem121
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = Int32.MaxValue;
+            int maxProfit = 0;
+
+            // Loop through the list of the prices
+            for (int i = 0; i < prices.Length; i++)
+            {
+                // Buying a stock
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+
+                // Selling a stock
+                if (prices[i] - minPrice > maxProfit)
+                {
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ LCChallengeSolutions/LCChallengeUnitTestProject/LCProblem121Test.cs
@@ -0,0 +1,37 @@
+﻿using LCChallengeSolutions;
+using Xunit;
+
+namespace LCChallengeUnitTestProject
+{
+    public class LCProblem121Test
+    {
+        int[] pricesList1 = new int[] { 7, 1, 5, 3, 6, 4 };
+        int[] pricesList2 = new int[] { 7, 6, 4, 3, 1 };
+
+        [Fact]
+        public void MaxProfit_ForPriceList1_ReturnMaxProfit()
+        {
+            // Arrange
+            var sut = new LCProblem121();
+
+            // Act
+            var result = sut.MaxProfit(pricesList1);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void MaxProfit_ForPriceList2_ReturnMaxProfit()
+        {
+            // Arrange
+            var sut = new LCProblem121();
+
+            // Act
+            var result = sut.MaxProfit(pricesList2);
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+    }
+}
