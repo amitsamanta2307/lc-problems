d5d6e040c6c2bc204c7ba959d879c8c159581eab
Amit Samanta <developer.amitsamanta@gmail.com>
2020-10-20 00:36:16 +0530

    Add new solution to the challenge, 1007

---
+++ LCChallengeSolutions/LCChallengeSolutions/LCProblem1007.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCChallengeSolutions
+{
+    public class LCProblem1007
+    {
+        /*
+         * Problem 1007
+         * Minimum Domino Rotations For Equal Row
+         * In a row of dominoes, A[i] and B[i] represent the top and bottom halves of the ith domino.
+         * (A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
+         * We may rotate the ith domino, so that A[i] and B[i] swap values.
+         * Return the minimum number of rotations so that all the values in A are the same, or all the values in B are the same.
+         * If it cannot be done, return -1.
+         */
+
+        public int MinDominoRotations(int[] A, int[] B)
+        {
+            int result = Math.Min(GetMinimumRotations(A[0], A, B), GetMinimumRotations(A[0], B, A));
+            result = Math.Min(result, GetMinimumRotations(A[0], A, B));
+            result = Math.Min(result, GetMinimumRotations(A[0], B, A));
+
+            return result == Int32.MaxValue ? -1 : result;
+        }
+
+        protected int GetMinimumRotations(int target, int[] A, int[] B)
+        {
+            int minSwaps = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] != target && B[i] != target)
+                {
+                    return Int32.MaxValue;
+                }
+                else if (A[i] != target)
+                {
+                    minSwaps++;
+                }
+            }
+
+            return minSwaps;
+        }
+    }
+}
---
+++ LCChallengeSolutions/LCChallengeUnitTestProject/LCProblem1007Test.cs
@@ -0,0 +1,38 @@
+﻿using LCChallengeSolutions;
+using Xunit;
+
+namespace LCChallengeUnitTestProject
+{
+    public class LCProblem1007Test
+    {
+        [Fact]
+        public void MinDominoRotations_ForDominos_ReturnMinSwaps()
+        {
+            // Arrange
+            int[] A = new int[] { 2, 1, 2, 4, 2, 2 };
+            int[] B = new int[] { 5, 2, 6, 2, 3, 2 };
+            var sut = new LCProblem1007();
+
+            // Act
+            var result = sut.MinDominoRotations(A, B);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void MinDominoRotations_ForDominos_ReturnNegativeOne()
+        {
+            // Arrange
+            int[] A = new int[] { 3, 5, 1, 2, 3 };
+            int[] B = new int[] { 3, 6, 3, 3, 4 };
+            var sut = new LCProblem1007();
+
+            // Act
+            var result = sut.MinDominoRotations(A, B);
+
+            // Assert
+            Assert.Equal(-1, result);
+        }
+    }
+}
